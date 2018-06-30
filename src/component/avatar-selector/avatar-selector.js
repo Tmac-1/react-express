328c4cc866f98b85c1c16bd4f3d8f10352beec90
Tmac-1 <1906526613@qq.com>
2018-06-30 11:28:56 +0800

    添加boss信息页面

---
+++ src/component/avatar-selector/avatar-selector.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Grid,List} from 'antd-mobile'
+
+class AvatarSelector extends React.Component{
+    state ={ }
+    render(){
+        const avatarList = 'ai,anxi,caizi,chimu,clay,cp3,gonchen,gorden,james,liuchan,mumu,nash,qingzi,sanjin,xiandao,yingmu'
+                           .split(',')
+                           .map(v=>({
+                               icon:require(`../img/${v}.png`),
+                               text:v
+                           }))
+        const girdHeader = this.state.icon ? 
+                           <div> 
+                               <span>已选头像</span>
+                               <img style={{width:20,height:20}} src={this.state.icon} alt=''/>
+                           </div> :'请选择头像';                
+        return (
+            <div>
+                 <List renderHeader={()=>girdHeader}>
+                    <Grid data={avatarList} columNum={4}
+                    onClick = { elm =>{
+                        this.setState(elm)
+                        this.props.selectAvatar(elm.text)
+                        }
+                    }
+                    />
+                 </List>
+
+            </div>
+        )
+    }
+}
+
+export default AvatarSelector;
---
+++ src/container/bossinfo/bossinfo.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {NavBar, InputItem,TextareaItem,Button} from 'antd-mobile';
+import AvatarSelector from '../../component/avatar-selector/avatar-selector'
+
+class BossInfo extends React.Component{
+    state={
+        title:'',
+        company:'',
+        money:'',
+        desc:'',
+        avatar:''
+    }
+
+    onChange = (key,val)=>{
+        this.setState({
+            [key]:val
+        })
+    }
+
+    render(){
+        return (
+            <div>
+                <NavBar
+                   mode="dark"
+                >
+                  BOSS完善信息页面
+                </NavBar>
+                 <AvatarSelector selectAvatar={(imgname)=>{
+                     this.setState({
+                         avatar:imgname
+                     })
+                 }}/>
+                 <InputItem onChange={(v)=>this.onChange('title',v)}>
+                    招聘职位
+                 </InputItem>
+                 <InputItem onChange={(v)=>this.onChange('company',v)}>
+                    公司名称
+                 </InputItem>
+                 <InputItem onChange={(v)=>this.onChange('money',v)}>
+                    职位薪资
+                 </InputItem>
+
+                 <TextareaItem 
+                   rows={3}
+                   autoHeight
+                   title='职位要求'
+                   onChange={(v)=>this.onChange('desc',v)}>
+                    
+                 </TextareaItem>
+                 <Button type='primary' > 保存 </Button>
+            </div>
+        )
+    }
+}
+
+export default BossInfo;
