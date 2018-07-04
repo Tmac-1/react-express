a2835a56870b6d6e29926cdc9f17c6d523c10869
Tmac-1 <1906526613@qq.com>
2018-07-04 12:03:52 +0800

    添加牛人列表应用骨架

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {createStore,applyMiddleware,compose} from 'redux';
 import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
-import {BrowserRouter,Route} from 'react-router-dom';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
 import Register from './container/register/register';
 import Login from './container/login/login';
 import BossInfo from './container/bossinfo/bossinfo';
 import Geniusinfo from './container/geniusinfo/geniusinfo';
-import reducers from './reducer';
 import AuthRoute from './component/anthRoute/anthRoute';
+import Dashbord from "./container/dashboard/dashboard";
+import reducers from './reducer';
 import './config';
 import 'antd-mobile/dist/antd-mobile.css';
 import './index.css';
@@ -23,22 +24,22 @@ const store = createStore(reducers,compose(
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
-function Boss(){
-  return (
-      <h2>这是boss页面</h2>
-  )
-}
 
+
+// boss genius me msg 4个页面
 ReactDOM.render(
         <Provider store={store}>
              <BrowserRouter>
                <div>
-                   <AuthRoute></AuthRoute>
-                   <Route path='/bossinfo' component={BossInfo}/>
-                   <Route path='/geniusinfo' component={Geniusinfo}/>
-                   <Route path='/boss' component={Boss}/>
-                   <Route path='/login' component={Login}/>
-                   <Route path='/register' component={Register}/>
+                  <AuthRoute></AuthRoute>
+                  <Switch>
+                    <Route path='/bossinfo' component={BossInfo}/>
+                    <Route path='/geniusinfo' component={Geniusinfo}/>
+                    <Route path='/login' component={Login}/>
+                    <Route path='/register' component={Register}/>
+                    <Route component={Dashbord}/>  
+                  </Switch>
+
                </div>
              </BrowserRouter>
         </Provider>,
